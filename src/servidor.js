fbdc0d393e8bdce5d50bd2c1341449d86eb4cf6a
Thiago Dannemann <tdannemreis@gmail.com>
2024-03-06 13:04:46 -0300

    adicionando configurações externas

--- src/servidor.js
+++ src/servidor.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const rotas = require('./rotas');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.use(rotas);
